f314888fef84ba7391d1a6e2c1cece88b648322b
sergelorenz <sergelorenz.villasica@gmail.com>
2022-02-15 22:38:11 +0800

    added grid using state of numLetters

--- client/src/components/GameArea/LetterGrid/Cell/Cell.js
+++ client/src/components/GameArea/LetterGrid/Cell/Cell.js
@@ -1,12 +1,29 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-const Cell = () => {
+import './Cell.scss';
+
+const Cell = ({key, cellId}) => {
   return (
-    <div>Cell</div>
+    <div className="cell" key={key} id={cellId}>
+      <div className="cell-letter" />
+      <div className="cell-feedback" />
+    </div>
   )
 }
 
-Cell.propTypes = {}
+Cell.propTypes = {
+  key: PropTypes.number.isRequired,
+  cellId: PropTypes.string.isRequired
+}
 
+/*
+const mapStateToProps = state = ({
+  letter: state.letterGrid.letter,
+  letterFeedback: state.letterGrid.letterFeedback
+})
+
+export default connect(mapStateToProps, {})(Cell)
+*/
 export default Cell
