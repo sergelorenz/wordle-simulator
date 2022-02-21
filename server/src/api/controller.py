665106f889697a4c57172e46fc2903c9d6f17d3a
sergelorenz <sergelorenz.villasica@gmail.com>
2022-02-21 16:32:02 +0800

    api update for pickRandomWord

--- server/src/api/controller.py
+++ server/src/api/controller.py
@@ -1,5 +1,9 @@
-from src import app
 from flask import jsonify, request
+from src import app
+
+from src.util import wordle_util
+
+from config import RESP_OK
 
 
 @app.route('/')
@@ -9,4 +13,8 @@ def index():
 
 @app.route('/pickRandomWord', methods=['GET'])
 def pick_random_word():
-    
+    if request.method == 'GET':
+        data = request.get_json()
+        num_letters = data['num_letters']
+        chosen_word = wordle_util.pick_word(num_letters)
+        return jsonify({'chosen_word': chosen_word}), RESP_OK
