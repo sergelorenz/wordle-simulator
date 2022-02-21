2f38db84af721549ec5d18e3e8d94c0e69279050
sergelorenz <sergelorenz.villasica@gmail.com>
2022-02-21 19:00:01 +0800

    api for grading of a guess

--- server/src/api/controller.py
+++ server/src/api/controller.py
@@ -18,3 +18,13 @@ def pick_random_word():
         num_letters = data['num_letters']
         chosen_word = wordle_util.pick_word(num_letters)
         return jsonify({'chosen_word': chosen_word}), RESP_OK
+
+    
+@app.route('/gradeGuess', methods=['POST'])
+def grade_guess():
+    if request.method == 'POST':
+        data = request.get_json()
+        guess = data['guess']
+        answer = data['answer']
+        grade = wordle_util.grade_word(guess, answer)
+        return jsonify({'grade': grade}), RESP_OK
